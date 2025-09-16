f5e3216d46004e0c6b1b612c994793bc0e1839ec
shivam G20sql <shivam9hjp@gmail.com>
2025-09-16 16:08:35 +0000

    revert the changes

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
-      },
+      },,
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com', // ✅ allow Firebase images
